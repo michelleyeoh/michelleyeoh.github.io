82a6e94e9accb325a2929473d578a903a63dea49
Michelle <michellew.yeoh@gmail.com>
2024-05-23 17:46:55 -0700

    details

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -1,15 +1,18 @@
 import "../styles/projectCard.css"
 import { Link } from "react-router-dom"
 
-export function ProjectCard({title, description, time, tool, linkUrl, linkTitle, img}){
+export function ProjectCard({title, description, time, tool, linkUrl, linkTitle, linkUrl2, linkTitle2, img}){
     return(
         <div className="rectangle">
             <div className="column">
-                <h1>{title}</h1>
+                <h1 style={{lineHeight:'70px'}}>{title}</h1>
                 <p>{description}</p>
                 <p>{time}</p>
                 <p>{tool}</p>
+                <br/>
                 <Link to={linkUrl} className="link">{linkTitle}</Link>
+                <br/>
+                <Link to={linkUrl2} className="link">{linkTitle2}</Link>
             </div>
             <div className="column">
                 <img src={img} alt={img} height={'250px'}/>

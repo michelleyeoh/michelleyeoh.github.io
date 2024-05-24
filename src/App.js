e8a894ab3ac8c68ecb2a809042b7bcc084db57d1
Michelle <michellew.yeoh@gmail.com>
2024-05-23 21:51:59 -0700

    retry router again

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Header from './components/header';
 import Footer from './components/footer';
 import Technical from './components/technical';
@@ -10,9 +10,9 @@ function App() {
     <div className='App'>
       <Header/>
         <Routes>
-          <Route path ="/" Component={Technical}/>
-          <Route path ="/technical" Component={Technical}/>
-          <Route path ="/design" Component={Design}/>
+          <Route path ="/" element={Technical}/>
+          <Route path ="/technical" element={Technical}/>
+          <Route path ="/design" element={Design}/>
         </Routes>
       <Footer/>
     </div>

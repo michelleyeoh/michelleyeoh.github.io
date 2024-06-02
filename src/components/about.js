67d2269903522ca2becb13d05f9454fa0e859fe2
Michelle <michellew.yeoh@gmail.com>
2024-06-02 15:11:07 -0700

    about chnages

--- src/components/about.js
+++ src/components/about.js
@@ -1,5 +1,8 @@
 import "../styles/about.css"
 import myeoh_profile from '../images/myeoh_profile.png'
+import github_icon from '../images/github_icon.png'
+import linkedin_icon from '../images/linkedin_icon.png'
+import { Link } from "react-router-dom"
 
 export default function About(){
     return(
@@ -10,25 +13,28 @@ export default function About(){
                 <h3>About Me</h3>
                 <h6>Hello, I’m Michelle!</h6>
                 <h6>I am a first-year at UC Davis with goals to pursue a career in software development. I am currently majoring in computer science, with a minor in technology management. </h6>
-                <h6>In addition to computer science, I also have a passion for UI/UX design. Sparked by my creative background in graphic design, I am also diving into web and mobile design.</h6>
-                <h6>During my free time like to spend time outside, taking photos and going on runs. I am also a big fan of Korean music, so I will always be found listening to music or having a concert in my room…</h6>
+                <h6>In addition to computer science, I also have a passion for UI/UX design. Sparked by my creative background in graphic design, I have been exploring web and mobile design.</h6>
+                <h6>During my free time like to spend time outside taking photos, studying at cafes, and going on runs. I am also a big fan of Korean music, so I will always be found listening to music or having a concert in my room…</h6>
                 <br/>
-
-                {/* <h3>Links</h3> */}
-
             </div>
             <div className="rightCol">
                 <h3>My Values</h3>
                 <h4>💬 Communication</h4>
-                <h6>Without communication, there is no team. Being transparent with one another will allow a collaborative environment where growth and knowledge can grow.</h6>
+                <h6>Without communication, there is no team. Being transparent with one another allows a collaborative environment where growth and knowledge can foster.</h6>
                 <h4>⚖️ Balance</h4>
-                <h6>Just like in nature, balance should also be reflected in other aspects. Work life, mental health, and physical health balance are all important when navigating through life. Finding a way to alleviate stress is important to regulate emotions.</h6>
+                <h6>Just like in nature, balance should also be reflected in other aspects: Work life, mental health, physical health balance, + more. Finding a way to alleviate stress is important when navigating through challenging times.</h6>
                 <br/>
 
                 <h3>Recent Experiences</h3>
                 <h6><strong>Technical Cohort</strong> at #include<br/><i>January 2024 - Present</i></h6>
                 {/* <h6><strong>Instructor</strong> at The LEAGUE of Amazing Programmers<br/><i>May 2024 - Present</i></h6> */}
                 <h6><strong>DI Fellowship</strong> at Davis Design Interactive<br/><i>April - May 2024</i></h6>
+                <Link target={"_blank"} to="https://www.linkedin.com/in/myeoh/">
+                    <img src={linkedin_icon} alt='linkedin_icon' style={{height:30, paddingRight:10}}/>
+                </Link>
+                <Link target={"_blank"} to="https://github.com/michelleyeoh">
+                    <img src={github_icon} alt='github_icon' style={{height:30}}/>
+                </Link>
             </div>
         </div>
         </div>
--- src/components/technical.js
+++ src/components/technical.js
@@ -12,7 +12,7 @@ export default function Technical(){
         <technical className="technical">
             <Landing/>
             <h2 id="technical">Explore my Projects</h2>
-            <ProjectCard title={"JASS"} description={"#include Technical Cohort"} time={"March 2024 - Present"} tool={"React.js + Next.js"} img={jass_landing} linkTitle={"In Progress"}/>
+            <ProjectCard title={"JASS"} description={"#include Technical Cohort"} time={"March 2024 - Present"} tool={"React.js + Next.js"} img={jass_landing} linkTitle={"In Progress"} linkUrl={"https://github.com/include-davis/jass-ws24"}/>
             <ProjectCard title={"DECA at UC Davis"} description={"Web Developer"} time={"March 2024 - Present"} tool={"React.js + Node.js"} img={deca_landing} linkTitle={"View GitHub →"} linkUrl={"https://github.com/sohamk0524/DECA-information-website"}/>
             <ProjectCard title={"Campus Connect"} description={"48 Hour Hackathon"} time={"April 2024"} tool={"React.js, Node.js, Firebase, mySQL, Sequelize, Express.js"} img={campusconnect_landing} linkTitle={"View Devpost →"} linkUrl={"https://devpost.com/software/campusconnect-orenh0"}/>
             <ProjectCard title={"Minion Bananaria"} description={"AP CSA Final Project"} time={"May - June 2023"} tool={"Java Swing"} img={minion_landing} linkTitle={"View GitHub →"} linkUrl={"https://github.com/vidhitapde/MINIONBANANARIA_FINAL"}/>

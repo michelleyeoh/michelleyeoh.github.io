fd67019800fd67d5f32bbed56a06d4b8da061a83
michelleyeoh <michellew.yeoh@gmail.com>
2024-07-31 15:37:30 -0700

    new landing ui

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+// import { useState } from "react"
 import { Link } from "react-router-dom"
 import github_icon from '../images/github_icon.png'
 import linkedin_icon from '../images/linkedin_icon.png'

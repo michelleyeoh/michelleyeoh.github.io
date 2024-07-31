57eea7288ee2543d68571b831256b2134d799fdd
Michelle <michellew.yeoh@gmail.com>
2024-07-31 12:57:16 -0700

    new landing

--- src/components/header.js
+++ src/components/header.js
@@ -1,29 +1,53 @@
 import { useState } from "react"
+import { Link } from "react-router-dom"
+import github_icon from '../images/github_icon.png'
+import linkedin_icon from '../images/linkedin_icon.png'
 import "../styles/header.css"
 
 export default function Header(){
-
-    const [color, setColor] = useState(false);
-    const changeColor = () => {
-        if(window.scrollY >= window.innerHeight){
-            setColor(true)
-        } else if (window.location.hash==='#about'){
-            setColor(true)
-        } else {
-            setColor(false)
-        }
-    }
-    window.addEventListener('scroll', changeColor)
-    return(
-        <header className={color ? 'header-bg' :'header'}>
-            <nav>
-                <a href="#technical">TECHNICAL</a>
-                <a href="#design">DESIGN</a>
-                <a href="/#about">ABOUT</a>
-                {/* <a href="/#/technical">TECHNICAL</a>
-                <a href="/#/design">DESIGN</a> */}
-                {/* <a href="/#/about">ABOUT</a> */}
+    return (
+        <header>
+            <nav className="header2">
+                <div className="header2_left">
+                    <a href="#technical">TECHNICAL</a>
+                    <a href="#design">DESIGN</a>
+                    <a href="/#about">ABOUT</a>
+                </div>
+                <div className="header2_center">
+                    <a href="/">MICHELLE YEOH</a>
+                </div>
+                <div className="header2_right">
+                    <Link target={"_blank"} to="https://www.linkedin.com/in/myeoh/">
+                        <img src={linkedin_icon} alt='linkedin_icon' />
+                    </Link>
+                    <Link target={"_blank"} to="https://github.com/michelleyeoh">
+                        <img src={github_icon} alt='github_icon'/>
+                    </Link>
+                </div>
             </nav>
         </header>
     )
+    // const [color, setColor] = useState(false);
+    // const changeColor = () => {
+    //     if(window.scrollY >= window.innerHeight){
+    //         setColor(true)
+    //     } else if (window.location.hash==='#about'){
+    //         setColor(true)
+    //     } else {
+    //         setColor(false)
+    //     }
+    // }
+    // window.addEventListener('scroll', changeColor)
+    // return(
+    //     <header className={color ? 'header-bg' :'header'}>
+    //         <nav>
+    //             <a href="#technical">TECHNICAL</a>
+    //             <a href="#design">DESIGN</a>
+    //             <a href="/#about">ABOUT</a>
+    //             {/* <a href="/#/technical">TECHNICAL</a>
+    //             <a href="/#/design">DESIGN</a> */}
+    //             {/* <a href="/#/about">ABOUT</a> */}
+    //         </nav>
+    //     </header>
+    // )
 }
--- src/components/landing.js
+++ src/components/landing.js
@@ -2,23 +2,35 @@ import React from 'react';
 import Typewriter from '../Typewriter';
 import '../styles/landing.css';
 import downButton from '../images/downButton.png'
+import landing_technical from '../images/landing_technical.png'
 
 export function Landing() {
-    const textArray = [
-        { text: "Hello! 👋", style: { color: '#827057' } },
-        { text: "I'm Michelle Yeoh.", style: { color: 'black' } },
-        { text: "Aspiring Software / Web Developer.", style: { color: 'black' } }
-    ];
-
     return (
-        <div className="landing">
-            <h2 style={{ color: 'black' }}>
-                <Typewriter textArray={textArray} delay={50} lineDelay={150} />
-            </h2>
-            <a href="#technical">
-                <img src={downButton} height={40} width={40} alt='down'/>
-            </a>
-            
+        <div className="landing2">
+            <div className='landing2_text'>
+                <h1 style={{color:'white', fontSize:'130px'}}>TECHNICAL</h1>
+                
+                <a href="#technical" style={{fontSize:'20px'}}>EXPLORE</a>
+            </div>
+            <img src={landing_technical} alt="landing technical"/>
         </div>
-    );
+
+    )
+    // const textArray = [
+    //     { text: "Hello! 👋", style: { color: '#827057' } },
+    //     { text: "I'm Michelle Yeoh.", style: { color: 'black' } },
+    //     { text: "Aspiring Software / Web Developer.", style: { color: 'black' } }
+    // ];
+
+    // return (
+    //     <div className="landing">
+    //         <h2 style={{ color: 'black' }}>
+    //             <Typewriter textArray={textArray} delay={50} lineDelay={150} />
+    //         </h2>
+    //         <a href="#technical">
+    //             <img src={downButton} height={40} width={40} alt='down'/>
+    //         </a>
+            
+    //     </div>
+    // );
 }

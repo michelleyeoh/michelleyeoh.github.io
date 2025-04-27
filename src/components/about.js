3edb02bc0961f21b0c40c0022b3312f5dd32f7ec
michelleyeoh <michellew.yeoh@gmail.com>
2025-04-27 16:10:15 -0700

    about page refactor and home page change

--- src/components/about.js
+++ src/components/about.js
@@ -7,42 +7,30 @@ import { Link } from "react-router-dom"
 export default function About() {
     return (
         <div className="about" id="about">
-            <div className="box">
-                <div className="leftCol">
-                    <img src={myeoh_profile} alt="myeoh_profile" />
-                    {/* <h3>My Values</h3>
-                <h4>💬 Communication</h4>
-                <h6>Transparency fosters a collaborative environment of growing knowledge.</h6>
-                <h4>👥 Collaboration</h4>
-                <h6>Learning with and from one another.</h6>
-                <h4>⚖️ Balance</h4>
-                <h6>Finding a way to alleviate stress when navigating through challenging times.</h6>
-                <br/> */}
+            <img src={myeoh_profile} alt="myeoh_profile" className="profile" />
+            <div className="container">
+                <div className="half">
                     <h2>About Me</h2>
                     <h6>Hello, I’m Michelle!</h6>
                     <h6>I am a second-year at UC Davis with goals to pursue a career in software development. I am currently majoring in <strong>computer science</strong>, with a minor in technology management. </h6>
-                    <h6>Sparked by my creative background in graphic design, I am also exploring <strong>UI/UX design</strong> for web and mobile devices.</h6>
+                    <h6>I have several years of experience in full-stack web devlopment through various opportunities on campus such as HackDavis, #include, and creating websites for clubs. In addition, I have a creative background in graphic design and am also exploring <strong>UI/UX design</strong> for web and mobile devices.</h6>
                     <h6>During my free time I like to spend time outside taking <strong>photos</strong>, studying at <strong>cafes</strong>, and going on <strong>runs</strong>. I also enjoy Korean music, so I will be found either listening to K-Pop or having a solo concert in my room…</h6>
-                    <br />
-
+                    {/* <br /> */}
                 </div>
-                <div className="rightCol">
+                <div className="half">
                     <h3>Recent Experiences</h3>
                     <h6><strong>Software Development Engineer Intern</strong> at Amazon<br /><i>Summer 2025</i></h6>
                     <h6><strong>Technical Director</strong> at HackDavis<br /><i>June 2024 - Present</i></h6>
-                    {/* <h6><strong>Instructor</strong> at The LEAGUE of Amazing Programmers<br/><i>May 2024 - Present</i></h6> */}
                     <h6><strong>Technical Lead</strong> at #include<br /><i>May 2024 - March 2025</i></h6>
                     <h6><strong>Design Director</strong> at Women in Computer Science<br /><i>May 2024 - Present</i></h6>
                     <h6><strong>Technical Cohort</strong> at #include<br /><i>January - June 2024</i></h6>
                     {/* <h6><strong>DI Fellowship</strong> at Davis Design Interactive<br /><i>April - May 2024</i></h6> */}
-
                     <Link target={"_blank"} to="https://www.linkedin.com/in/myeoh/">
                         <img src={linkedin_icon} alt='linkedin_icon' style={{ height: 30, paddingRight: 10 }} />
                     </Link>
                     <Link target={"_blank"} to="https://github.com/michelleyeoh">
                         <img src={github_icon} alt='github_icon' style={{ height: 30 }} />
                     </Link>
-
                 </div>
             </div>
         </div>
--- src/components/landing.js
+++ src/components/landing.js
@@ -1,60 +1,60 @@
 import React from 'react';
-// import Typewriter from '../Typewriter';
+import Typewriter from '../Typewriter';
 import '../styles/landing.css';
-// import downButton from '../images/downButton.png'
-import { useEffect, useState } from 'react';
-import landing_technical from '../images/landing_technical.png'
-import landing_design from '../images/landing_design.png'
+import downButton from '../images/downButton.png'
+// import { useEffect, useState } from 'react';
+// import landing_technical from '../images/landing_technical.png'
+// import landing_design from '../images/landing_design.png'
 
 export function Landing() {
-    const items = [
-        { title: 'TECHNICAL', link: '#technical' ,img: landing_technical},
-        { title: 'DESIGN', link: '#design', img: landing_design },
-      ];
-    
-      const [currentIndex, setCurrentIndex] = useState(0);
-      const [fadeClass, setFadeClass] = useState('fade-in');
-  
-      useEffect(() => {
-          const interval = setInterval(() => {
-              setFadeClass('fade-out');
-              setTimeout(() => {
-                  setCurrentIndex((prevIndex) => (prevIndex + 1) % items.length);
-                  setFadeClass('fade-in');
-              }, 1000); // Duration of fade-out effect
-          }, 6000); // Interval between transitions, should be longer than the fade duration
-  
-          return () => clearInterval(interval);
-      }, [items.length]);
-  
-      const currentItem = items[currentIndex];
-
-    return (
-        <div className="landing-container">
-            <div className={`landing2 ${fadeClass}`}>
-                <div className="landing2_text">
-                    <h1>{currentItem.title}</h1>
-                    <a href={currentItem.link}>EXPLORE</a>
-                </div>
-                <img src={currentItem.img} alt="landing_img" />
-            </div>
-        </div>
-    );
-    // const textArray = [
-    //     { text: "Hello! 👋", style: { color: '#827057' } },
-    //     { text: "I'm Michelle Yeoh.", style: { color: 'black' } },
-    //     { text: "Aspiring Software / Web Developer.", style: { color: 'black' } }
+    // const items = [
+    //     { title: 'TECHNICAL', link: '#technical', img: landing_technical },
+    //     { title: 'DESIGN', link: '#design', img: landing_design },
     // ];
 
+    // const [currentIndex, setCurrentIndex] = useState(0);
+    // const [fadeClass, setFadeClass] = useState('fade-in');
+
+    // useEffect(() => {
+    //     const interval = setInterval(() => {
+    //         setFadeClass('fade-out');
+    //         setTimeout(() => {
+    //             setCurrentIndex((prevIndex) => (prevIndex + 1) % items.length);
+    //             setFadeClass('fade-in');
+    //         }, 1000); // Duration of fade-out effect
+    //     }, 6000); // Interval between transitions, should be longer than the fade duration
+
+    //     return () => clearInterval(interval);
+    // }, [items.length]);
+
+    // const currentItem = items[currentIndex];
+
     // return (
-    //     <div className="landing">
-    //         <h2 style={{ color: 'black' }}>
-    //             <Typewriter textArray={textArray} delay={50} lineDelay={150} />
-    //         </h2>
-    //         <a href="#technical">
-    //             <img src={downButton} height={40} width={40} alt='down'/>
-    //         </a>
-            
+    //     <div className="landing-container">
+    //         <div className={`landing2 ${fadeClass}`}>
+    //             <div className="landing2_text">
+    //                 <h1>{currentItem.title}</h1>
+    //                 <a href={currentItem.link}>EXPLORE</a>
+    //             </div>
+    //             <img src={currentItem.img} alt="landing_img" />
+    //         </div>
     //     </div>
     // );
+    const textArray = [
+        { text: "Hello! 👋", style: { color: '#827057' } },
+        { text: "I'm Michelle Yeoh.", style: { color: 'black' } },
+        { text: "Aspiring Software Developer.", style: { color: 'black' } }
+    ];
+
+    return (
+        <div className="landing">
+            <h2 style={{ color: 'black' }}>
+                <Typewriter textArray={textArray} delay={50} lineDelay={150} />
+            </h2>
+            <a href="#technical">
+                <img src={downButton} height={40} width={40} alt='down' />
+            </a>
+
+        </div>
+    );
 }

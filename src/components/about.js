39d29d70aecdbcff033fc05c7103fb8329d45c0a
michelleyeoh <michellew.yeoh@gmail.com>
2025-04-26 23:19:04 -0700

    header update

--- src/components/about.js
+++ src/components/about.js
@@ -10,6 +10,23 @@ export default function About() {
             <div className="box">
                 <div className="leftCol">
                     <img src={myeoh_profile} alt="myeoh_profile" />
+                    {/* <h3>My Values</h3>
+                <h4>💬 Communication</h4>
+                <h6>Transparency fosters a collaborative environment of growing knowledge.</h6>
+                <h4>👥 Collaboration</h4>
+                <h6>Learning with and from one another.</h6>
+                <h4>⚖️ Balance</h4>
+                <h6>Finding a way to alleviate stress when navigating through challenging times.</h6>
+                <br/> */}
+                    <h2>About Me</h2>
+                    <h6>Hello, I’m Michelle!</h6>
+                    <h6>I am a second-year at UC Davis with goals to pursue a career in software development. I am currently majoring in <strong>computer science</strong>, with a minor in technology management. </h6>
+                    <h6>Sparked by my creative background in graphic design, I am also exploring <strong>UI/UX design</strong> for web and mobile devices.</h6>
+                    <h6>During my free time I like to spend time outside taking <strong>photos</strong>, studying at <strong>cafes</strong>, and going on <strong>runs</strong>. I also enjoy Korean music, so I will be found either listening to K-Pop or having a solo concert in my room…</h6>
+                    <br />
+
+                </div>
+                <div className="rightCol">
                     <h3>Recent Experiences</h3>
                     <h6><strong>Software Development Engineer Intern</strong> at Amazon<br /><i>Summer 2025</i></h6>
                     <h6><strong>Technical Director</strong> at HackDavis<br /><i>June 2024 - Present</i></h6>
@@ -25,23 +42,6 @@ export default function About() {
                     <Link target={"_blank"} to="https://github.com/michelleyeoh">
                         <img src={github_icon} alt='github_icon' style={{ height: 30 }} />
                     </Link>
-
-                </div>
-                <div className="rightCol">
-                    {/* <h3>My Values</h3>
-                <h4>💬 Communication</h4>
-                <h6>Transparency fosters a collaborative environment of growing knowledge.</h6>
-                <h4>👥 Collaboration</h4>
-                <h6>Learning with and from one another.</h6>
-                <h4>⚖️ Balance</h4>
-                <h6>Finding a way to alleviate stress when navigating through challenging times.</h6>
-                <br/> */}
-                    <h2>About Me</h2>
-                    <h6>Hello, I’m Michelle!</h6>
-                    <h6>I am a second-year at UC Davis with goals to pursue a career in software development. I am currently majoring in <strong>computer science</strong>, with a minor in technology management. </h6>
-                    <h6>Sparked by my creative background in graphic design, I am also exploring <strong>UI/UX design</strong> for web and mobile devices.</h6>
-                    <h6>During my free time I like to spend time outside taking <strong>photos</strong>, studying at <strong>cafes</strong>, and going on <strong>runs</strong>. I also enjoy Korean music, so I will be found either listening to K-Pop or having a solo concert in my room…</h6>
-                    <br />
 
                 </div>
             </div>
--- src/components/header.js
+++ src/components/header.js
@@ -2,26 +2,27 @@
 import { Link } from "react-router-dom"
 import github_icon from '../images/github_icon.png'
 import linkedin_icon from '../images/linkedin_icon.png'
+import logo from '../images/logo.png'
 import "../styles/header.css"
 
-export default function Header(){
+export default function Header() {
     return (
         <header>
             <nav className="header2">
+                <div className="header2_center">
+                    <a href="/"><img src={logo} alt='michelle Yeoh logo' /></a>
+                </div>
                 <div className="header2_left">
                     <a href="#technical">TECHNICAL</a>
-                    <a href="#design">DESIGN</a>
+                    <a href="#design" className="mobile_removed">DESIGN</a>
                     <a href="/#about">ABOUT</a>
-                </div>
-                <div className="header2_center">
-                    <a href="/">MICHELLE YEOH</a>
                 </div>
                 <div className="header2_right">
                     <Link target={"_blank"} to="https://www.linkedin.com/in/myeoh/">
                         <img src={linkedin_icon} alt='linkedin_icon' />
                     </Link>
                     <Link target={"_blank"} to="https://github.com/michelleyeoh">
-                        <img src={github_icon} alt='github_icon'/>
+                        <img src={github_icon} alt='github_icon' />
                     </Link>
                 </div>
             </nav>
